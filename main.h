ef15e78ff1c487f1968a09e88cfea1f4e063187e
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-10-05 09:05:07 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,31 +1,44 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<string>
+#include<map>
+#include<list>
+#include<set>
 #include<cmath>
-#include <iostream>
+#include<iostream>
 #include<bits/stdc++.h>
 
-//using namespace std;
-
-int add_digits(double num1, double num2){
+class Product {
+private:
    // put your code here
-   return num1 + num2;
+public:
+   // put your code here
+};
+
+class Order {
+private:
+   // put your code here
+public:
+   // put your code here
+};
+
+class Customer {
+private:
+   // put your code here
+public:
+   // put your code here
+};
+
+class RetailStore {
+private:
+   // put your code here
+public:
+   // put your code here
+};
+
+int main() {
+   // put your code here
+   return 0;
 }
 
-
-// int extract_decimal(double num){
-//     // put your code here
-// }
-
-// int sum_of_digits(int numb){
-// // put your code here
-// }
-
-// bool is_valid(double value){
-//     // call digits_count here and store output
-//     // call extract_decimal here and store output
-//     // call sum_of_digits here and store output
-
-//     // compare output of sum_of_digits and digits_count here
-// }
 #endif // MAIN_H
